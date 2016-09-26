7c62a98348326559f3388f57f9ff4835b20747e8
ZeroUnderscoreOu <ZeroUnderscoreOu@Yandex.ru>
2016-09-27 02:53:18 +0300

    Fixed broken update path Put "Greasemonkey" folder back to allow older users to update. Added ReadMe to prevent confusion. Added *.user.js as *.meta.js is not enough. Put all contents of *.user.js back as GM doesn't respect it without force update.

--- Greasemonkey/SteamBBCodes.user.js
+++ Greasemonkey/SteamBBCodes.user.js
@@ -1,5 +1,22 @@
 // ==UserScript==
+// @name        Steam BBCodes
+// @author      ZeroUnderscoreOu
 // @version     1.3.3
+// @icon        https://raw.githubusercontent.com/ZeroUnderscoreOu/SteamBBCodes/master/Logo128.png
+// @description Steam BBCodes editor
+// @namespace   https://github.com/ZeroUnderscoreOu/
+// @include     /^https?:\/\/steamcommunity.com\/(app\/\d+\/discussions|discussions\/forum|news\/post)\/.*/
+// @include     /^https?:\/\/steamcommunity.com\/sharedfiles\/(filedetails|edititem|itemedittext)\/.*/
+// @include     /^https?:\/\/steamcommunity.com\/(id|profiles)\/[^\/]+\/($|allcomments|edit|friendactivitydetail|home(?!\/invites)|recommended|status).*/
+// @include     /^https?:\/\/steamcommunity.com\/groups\/([^#\/]+)(#?$|#comments|#events|\/edit|\/discussions|\/reporteddiscussions).*/
+// @include     /^https?:\/\/steamcommunity.com\/groups\/([^#\/]+)(#|\/)announcements\/(create|detail|edit).*/
+// @match       *://steamcommunity.com/games/*/announcements/detail/*
+// @match       *://store.steampowered.com/app/*
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_getResourceText
+// @require     https://raw.githubusercontent.com/ZeroUnderscoreOu/SteamBBCodes/master/Beholder.js
+// @resource    SteamBBCodes https://raw.githubusercontent.com/ZeroUnderscoreOu/SteamBBCodes/master/SteamBBCodes.js
 // @downloadURL https://github.com/ZeroUnderscoreOu/SteamBBCodes/raw/master/Userscript/SteamBBCodes.user.js
 // @updateURL   https://github.com/ZeroUnderscoreOu/SteamBBCodes/raw/master/Userscript/SteamBBCodes.user.js
 // ==/UserScript==
