ee23bcf3717fdb3264fdfc7f32fc61c362aaae40
ZeroUnderscoreOu <ZeroUnderscoreOu@Yandex.ru>
2016-09-26 23:40:27 +0300

    Fixed broken update path Put "Greasemonkey" folder back to allow older users to update. Added ReadMe to prevent confusion. Added *.user.js as *.meta.js is not enough.

---
+++ Greasemonkey/SteamBBCodes.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @version     1.3.3
+// @downloadURL https://github.com/ZeroUnderscoreOu/SteamBBCodes/raw/master/Userscript/SteamBBCodes.user.js
+// @updateURL   https://github.com/ZeroUnderscoreOu/SteamBBCodes/raw/master/Userscript/SteamBBCodes.user.js
+// ==/UserScript==
+
+// Do not install from here.
+// Please delete previous version if you have it.
+// Install from new path:
+// https://github.com/ZeroUnderscoreOu/SteamBBCodes/raw/master/Userscript/SteamBBCodes.user.js
